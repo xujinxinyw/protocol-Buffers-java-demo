6d3aa9e2a129d9cd438c8907b2f16fb5e2533fd1
xujinxin <xujinxin@ahrayo.com>
2026-05-11 17:05:39 +0800

    demo

--- src/main/java/edu/sunrise/Main.java
+++ src/main/java/edu/sunrise/Main.java
@@ -1,17 +1,23 @@
 package edu.sunrise;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import edu.sunrise.context.RequestContext;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        byte[] msg = RequestContext.newBuilder()
+                .setId(1).setName("张三")
+                .setEmail("zhangsan@rayo.com")
+                .build().toByteArray();
+
+        try {
+            RequestContext requestContext = RequestContext.parseFrom(msg);
+            System.out.println(requestContext.toString());
+        } catch (InvalidProtocolBufferException e) {
+            throw new RuntimeException(e);
         }
     }
 }
---
+++ src/main/java/edu/sunrise/context/Test.java
@@ -0,0 +1,11 @@
+package edu.sunrise.context;
+
+/**
+ * 测试
+ *
+ * @author xujinxin
+ * @version 1.0
+ * @date 2026-05-11 16:46
+ */
+public class Test {
+}
